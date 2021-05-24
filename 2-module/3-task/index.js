06cb98368c174e0463b394f1a5ba444259b3c28a
Apelchinka <koitloit5@gmail.com>
2021-05-24 15:31:51 +0300

    калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
   // ваш код
+  read(a, b){
+    this.a = a;
+    this.b = b;
+  },
+  sum(){
+    let resultSum = this.a + this.b;
+    return resultSum;
+  },
+  mul(){
+    let resultMul = this.a * this.b;
+    return resultMul;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
