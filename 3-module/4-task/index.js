b96627554452ddbadba87a26c03d5d4c5c03e975
Apelchinka <koitloit5@gmail.com>
2021-05-30 17:10:29 +0300

    Функция showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,15 @@
 function showSalary(users, age) {
   // ваш код...
+    result = "";
+    users.forEach(function(user) {
+      if(user.age <= age) {
+        if (result == "") {
+          result += `${user.name}, ${user.balance}`;
+        } else {
+         result += `\n${user.name}, ${user.balance}`;
+        };
+      }
+    });
+    return result;
+
 }
