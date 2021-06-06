a563cbc0fc6035ad5c0f0d8568165f0f3b038e15
Apelchinka <koitloit5@gmail.com>
2021-06-06 23:17:19 +0300

    Скрыть элемент по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
   // ваш код...
+  let button = document.getElementsByClassName("toggle-text-button")[0];
+  let someText = document.getElementById("text");
+  button.onclick = function () {
+    if (someText.hidden == false) {
+      someText.hidden = true;
+    } else {
+      someText.hidden = false;
+    }
+  };
 }
